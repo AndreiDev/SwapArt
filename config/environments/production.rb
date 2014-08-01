8dc92c7ff936b89942ed7cbaf3f259ba4c85dcaa
AndreiDev <andrei.averkin@gmail.com>
2014-08-01 18:45:34 +0300

    updates for heroku db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Tur::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => Rails.application.secrets.gmail_user,
+      :password => Rails.application.secrets.gmail_pass,
+      :authentication => 'plain',
+      :enable_starttls_auto => true}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
