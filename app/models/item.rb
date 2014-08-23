029ff7327ae83fbae0f481215505284abd33d41c
AndreiDev <andrei.averkin@gmail.com>
2014-08-23 08:52:22 +0300

    added some validations to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,13 @@ class Item < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :image, :less_than => 4.megabytes, :more_than => 0.2.megabytes
-  validates :image, :presence => {:message => "נא להעלות תמונה"}
+  validates :image_file_name, presence: true
+  validates :original_price, :presence => {:message => ""}
+  validates :height, :presence => {:message => ""}
+  validates :width, :presence => {:message => ""}
+  validates :type, :presence => {:message => ""}
+  validates :age, :presence => {:message => ""}
+  validates :state, :presence => {:message => ""}
 
   belongs_to :user
   belongs_to :type
