34ebd72a7020cce42bb6f77b325ec775349bdbea
AndreiDev <andrei.averkin@gmail.com>
2014-09-05 09:17:54 +0300

    working of the states

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,5 +1,6 @@
 class GalleryController < ApplicationController
   def index
+
     users_that_want_my_items = User.find_by_sql ["SELECT DISTINCT users.*
     FROM users
     LEFT JOIN wants on users.id = wants.user_id
@@ -7,16 +8,28 @@ class GalleryController < ApplicationController
     WHERE users.is_active = 1 AND users.is_blocked = 0
     AND items.user_id = '?'", current_user.id]
 
-    @items_want_mine = Item.where.not(:user => current_user)
+    @items = Item.where.not(:user => current_user)
     .where(:is_blocked => false)
     .where(:is_active => true)
     .where.not(id: current_user.block_items.pluck(:id))
-    .where(user_id: users_that_want_my_items)
 
-    @items_dont_want_mine= Item.where.not(:user => current_user)
-    .where(:is_blocked => false)
-    .where(:is_active => true)
-    .where.not(id: current_user.block_items.pluck(:id))
-    .where.not(user_id: users_that_want_my_items)
+    @states = {}
+    @items.each { |item|
+      user_wants_item = item.want_users.include? current_user
+      item_owner_wants_users_item = users_that_want_my_items.include? item.user
+      case [user_wants_item, item_owner_wants_users_item]
+        when [false, false]
+          @states[item.id] = 1
+        when [true, false]
+          @states[item.id] = 2
+        when [false, true]
+          @states[item.id] = 3
+        when [true, true]
+          @states[item.id] = 4
+      end
+    }
+
+    @items.sort_by { |item| @states[item.id] }.reverse!
+
   end
 end
