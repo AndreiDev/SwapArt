9e3ee2960922ae88b486eeee8b936f5153fadc41
AndreiDev <andrei.averkin@gmail.com>
2014-10-02 23:10:46 +0300

    better "from" email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
-  # :mongoid (bson_ext recommended) by default. Other ORMs may be
+  # :mongoid (bson_ext recommended)ssh -vT git@github.com by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
 
