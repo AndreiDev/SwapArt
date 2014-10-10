4617a935fa843b16bc80a53c8e1aeccd2869834a
AndreiDev <andrei.averkin@gmail.com>
2014-10-10 21:14:14 +0300

    added user creation test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,4 +2,13 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
+  test "should create new user" do
+    user = User.new(email: 'ilana.averkin@gmail.com', first_name: 'אילנה', last_name: 'אברקין',
+                      password: 'abcd1234', password_confirmation: 'abcd1234')
+    user.skip_confirmation!
+    user.save!
+    user.add_role 'admin'
+    assert user.persisted?
+    assert_equal(user.get_user_roles_name,'admin')
+  end
 end
