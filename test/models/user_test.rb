21f02b7da392e2659ec2e2079f3e5155d4e6c2f5
AndreiDev <andrei.averkin@gmail.com>
2014-11-18 07:22:33 +0200

    added user unit tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,4 +11,13 @@ class UserTest < ActiveSupport::TestCase
     assert user.persisted?
     assert_equal(user.get_user_roles_name,'admin')
   end
+
+  test "get_states_function_should_work_fine" do
+    User.current = users(:user_11)
+    assert_equal(User.get_states([Item.first, Item.second, Item.third]).values.to_s,"[1, 3, 4]")
+  end
+
+  test "get_items_by_user2_that_user1_wants_function_should_work_fine" do
+    assert_equal(User.get_items_by_user2_that_user1_wants(users(:user_21), users(:user_11))[0],items(:item_1))
+  end
 end
